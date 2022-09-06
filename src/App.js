3a3d1e03e7966ff1991ba37ba35a41360ec8c674
twyle <okothlyleochieng@gmail.com>
2022-09-06 15:36:57 +0300

    feat: created the single post page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TopBar from "./Components/topbar/TopBar";
 import Home from "./Pages/home/Home";
+import Single from "./Pages/single/Single";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Home />
+      <Single />
     </>
   );
 }
