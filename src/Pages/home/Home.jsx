1faf61d364657dd679b52184f85e4c41410b66c6
twyle <okothlyleochieng@gmail.com>
2022-09-06 13:12:50 +0300

    feat: created the sidebar.

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,11 +1,20 @@
 import Header from '../../Components/header/Header'
+import Posts from '../../Components/posts/Posts'
+import SideBar from '../../Components/sidebar/SideBar'
 import './home.css'
 
 export default function Home() {
   return (
-    <div className='home'>
-        <Header />
-        homepage
-    </div>
+      <>
+
+          <Header />
+          <div className="home">
+
+            <Posts/>
+            <SideBar />
+
+          </div>
+
+      </>
   )
 }
