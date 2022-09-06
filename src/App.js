daa7f212a1c7cc8710b6635c1e1ff7c91ac9bc66
twyle <okothlyleochieng@gmail.com>
2022-09-06 16:22:32 +0300

    feat: created the write article page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import TopBar from "./Components/topbar/TopBar";
 import Home from "./Pages/home/Home";
 import Single from "./Pages/single/Single";
+import Write from "./Pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Single />
+      <Write />
     </>
   );
 }
