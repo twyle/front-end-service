e7f90f3e1a3e8e37326e11c52336ef5ee279ba83
twyle <okothlyleochieng@gmail.com>
2022-09-06 18:02:08 +0300

    feat: created the settings page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import TopBar from "./Components/topbar/TopBar";
 import Home from "./Pages/home/Home";
+import Settings from "./Pages/settings/Settings";
 import Single from "./Pages/single/Single";
 import Write from "./Pages/write/Write";
 
 function App() {
   return (
     <>
-      <TopBar/>
-      <Write />
+      <TopBar />
+      <Settings />
     </>
   );
 }
